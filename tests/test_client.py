7782faa5d0309e69cd117a87edd2e05eddd97e95
ahmed14-cell <ahmedito14@gmail.com>
2020-12-31 17:13:32 +0300

    update testing files

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,14 +1,19 @@
 import unittest
 from client import NamedEntity
+from test_doubles import NerModelTestDouble
 
 class TestClient(unittest.TestCase):
 	
-	def test_get_ents_return_dict_given_empty_str(self):
-		ner=NamedEntity()
+	def test_get_ents_return_dict_given_empty_str_causes_empty_spacy_doc_ents(self):
+		model = NerModelTestDouble('eng')
+		model.return_doc_ents([])
+		ner=NamedEntity(model)
 		ents=ner.get_ents("")
 		self.assertIsInstance(ents, dict)
 	
-	def test_get_ents_return_list_given_nonempty_str(self):
-		ner=NamedEntity()
+	def test_get_ents_return_dict_given_nonempty_str(self):
+		model = NerModelTestDouble('eng')
+		model.return_doc_ents([])
+		ner=NamedEntity(model)
 		ents=ner.get_ents("Cleanliness is half of faith")
 		self.assertIsInstance(ents, dict)
