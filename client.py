75a650a000bff25225609b3e45530e3d9f02cb87
ahmed14-cell <ahmedito14@gmail.com>
2020-12-31 17:12:38 +0300

    created proxy data

--- client.py
+++ client.py
@@ -1,5 +1,9 @@
+import spacy
+
 class NamedEntity:
-    def __init__(self):
-        pass
+    def __init__(self, model):
+        self.model = model
+
     def get_ents(self, sentence):
+        #doc = self.model(sentence)
         return {}
